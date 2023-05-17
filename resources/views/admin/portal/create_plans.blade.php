359a1f186f761ff229e63bceee7d1d270fdfcf6d
MADHUR-GUPTA <madhurmawai@gmail.com>
2023-05-17 23:26:07 +0530

    plan form design

--- resources/views/admin/portal/create_plans.blade.php
+++ resources/views/admin/portal/create_plans.blade.php
@@ -15,10 +15,12 @@
                     <ul class="navbar-nav">
 
                         <li class="nav-item">
-                            <a href="{{ route('view_plans') }}" class="nav-link" href="">View Plans</a>
+                            <a href="{{ route('view_plans') }}" class="nav-link" href="">View
+                                Plans</a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('create_plans') }}" class="nav-link active" href="">Add Plan</a>
+                            <a href="{{ route('create_plans') }}" class="nav-link active"
+                                href="">Add Plan</a>
                         </li>
                     </ul>
                 </div>
@@ -29,24 +31,75 @@
 
     {{-- TABLE START --}}
 
-    <div class="row row-cols-1 justify-content-center align-items-center g-2 my-3 ">
-        <div class="col">
+    <div class="row row-cols-1 justify-content-center align-items-center g-2 my-3 px-5">
+        <div class="col px-5">
             <form action="">
-                <div class="mb-3">
-                  <label for="" class="form-label">Name</label>
-                  <input type="text"
-                    class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-                  <small id="helpId" class="form-text text-muted">Help text</small>
+                <div class=" input-group mb-1">
+                    <span class="input-group-text">Plan Name</span>
+                    <input type="text" class="form-control form-control-sm" name="plan_name" id=""
+                        aria-describedby="helpId" placeholder="">
                 </div>
 
-                <div class="mb-3">
-                  <label for="" class="form-label">Name</label>
-                  <input type="text"
-                    class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-                  <small id="helpId" class="form-text text-muted">Help text</small>
+                <div class=" input-group mb-1">
+                    <span class="input-group-text">Price</span>
+                    <input type="text" class="form-control form-control-sm" name="price">
+
                 </div>
 
-                
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 1*</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures_req"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 2</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 3</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 4</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 5</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 6</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 7</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 8</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 9</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text">Feautures 10</span>
+                    <input type="text" class="form-control form-control-sm" name="feautures[]"
+                        id="">
+                </div>
+
+
+
+
             </form>
 
         </div>
